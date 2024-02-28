bd9ec8e2258f69f745b5b61de8f234cc5af07a38
AkRao47 <159660084+AkRao47@users.noreply.github.com>
2024-02-28 20:25:30 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,7 +1,7 @@
 api_id = 24192661
 api_hash = "810a6dc0b2c2bf4db55db3a9ad999b56"
 bot_token = "6371494303:AAGMaUGJltbs9rBI9p3jPX77rBfPGpahvnc"
-auth_users = [5427627648]
+auth_users = [5427627648,6148938625]
 sudo_user = 5427627648
 log_channel = -1002130830455
 txt_channel = -1002130830455
